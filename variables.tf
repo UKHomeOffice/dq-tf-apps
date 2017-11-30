027664281ea2c842ac09f5c5d790580406810baa
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-11-30 14:40:02 +0000

    put variables into a seperate file

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "cidr_block" {}
+variable "public_subnet_cidr_block" {}
+variable "dqdb_apps_cidr_block" {}
+variable "ext_feed_apps_cidr_block" {}
+variable "data_ingest_apps_cidr_block" {}
+variable "data_pipe_apps_cidr_block" {}
+variable "mdm_apps_cidr_block" {}
+variable "int_dashboard_cidr_block" {}
+variable "ext_dashboard_cidr_block" {}
+variable "az" {}
+variable "name_prefix" {}
