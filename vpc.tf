8a7ea9d39ee03999f6d4f3c4ecb7c9a388d0b9c9
david-wirelab <info@wirelab.co.uk>
2017-11-30 09:43:06 +0000

    subnet name with tests

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,12 @@
 variable "cidr_block" {}
-variable "vpc_subnet1_cidr_block" {}
-variable "vpc_subnet2_cidr_block" {}
-variable "vpc_subnet3_cidr_block" {}
-variable "vpc_subnet4_cidr_block" {}
-variable "vpc_subnet5_cidr_block" {}
-variable "vpc_subnet6_cidr_block" {}
-variable "vpc_subnet7_cidr_block" {}
-variable "vpc_subnet8_cidr_block" {}
+variable "public_subnet_cidr_block" {}
+variable "dqdb_apps_cidr_block" {}
+variable "ext_feed_apps_cidr_block" {}
+variable "data_ingest_apps_cidr_block" {}
+variable "data_pipe_apps_cidr_block" {}
+variable "mdm_apps_cidr_block" {}
+variable "int_dashboard_cidr_block" {}
+variable "ext_dashboard_cidr_block" {}
 variable "az" {}
 variable "name_prefix" {}
 
@@ -30,90 +30,90 @@ resource "aws_internet_gateway" "AppsRouteToInternet" {
   }
 }
 
-resource "aws_subnet" "AppsSubnet1" {
+resource "aws_subnet" "public_subnet" {
   vpc_id                  = "${aws_vpc.appsvpc.id}"
-  cidr_block              = "${var.vpc_subnet1_cidr_block}"
+  cidr_block              = "${var.public_subnet_cidr_block}"
   map_public_ip_on_launch = true
   availability_zone       = "${var.az}"
 
   tags {
-    Name = "${local.name_prefix}subnet1"
+    Name = "${local.name_prefix}public-subnet"
   }
 }
 
-resource "aws_subnet" "AppsSubnet2" {
+resource "aws_subnet" "dqdb_apps" {
   vpc_id                  = "${aws_vpc.appsvpc.id}"
-  cidr_block              = "${var.vpc_subnet2_cidr_block}"
+  cidr_block              = "${var.dqdb_apps_cidr_block}"
   map_public_ip_on_launch = true
   availability_zone       = "${var.az}"
 
   tags {
-    Name = "${local.name_prefix}subnet2"
+    Name = "${local.name_prefix}dqdb-subnet"
   }
 }
 
-resource "aws_subnet" "AppsSubnet3" {
+resource "aws_subnet" "ext_feed_apps" {
   vpc_id                  = "${aws_vpc.appsvpc.id}"
-  cidr_block              = "${var.vpc_subnet3_cidr_block}"
+  cidr_block              = "${var.ext_feed_apps_cidr_block}"
   map_public_ip_on_launch = true
   availability_zone       = "${var.az}"
 
   tags {
-    Name = "${local.name_prefix}subnet3"
+    Name = "${local.name_prefix}ext-feed-subnet"
   }
 }
 
-resource "aws_subnet" "AppsSubnet4" {
+resource "aws_subnet" "data_ingest_apps" {
   vpc_id                  = "${aws_vpc.appsvpc.id}"
-  cidr_block              = "${var.vpc_subnet4_cidr_block}"
+  cidr_block              = "${var.data_ingest_apps_cidr_block}"
   map_public_ip_on_launch = true
   availability_zone       = "${var.az}"
 
   tags {
-    Name = "${local.name_prefix}subnet4"
+    Name = "${local.name_prefix}data-ingest-subnet"
   }
 }
 
-resource "aws_subnet" "AppsSubnet5" {
+resource "aws_subnet" "data_pipe_apps" {
   vpc_id                  = "${aws_vpc.appsvpc.id}"
-  cidr_block              = "${var.vpc_subnet5_cidr_block}"
+  cidr_block              = "${var.data_pipe_apps_cidr_block}"
   map_public_ip_on_launch = true
   availability_zone       = "${var.az}"
 
   tags {
-    Name = "${local.name_prefix}subnet5"
+    Name = "${local.name_prefix}data-pipe-subnet"
   }
 }
 
-resource "aws_subnet" "AppsSubnet6" {
+resource "aws_subnet" "mdm_apps" {
   vpc_id                  = "${aws_vpc.appsvpc.id}"
-  cidr_block              = "${var.vpc_subnet6_cidr_block}"
+  cidr_block              = "${var.mdm_apps_cidr_block}"
   map_public_ip_on_launch = true
   availability_zone       = "${var.az}"
 
   tags {
-    Name = "${local.name_prefix}subnet6"
+    Name = "${local.name_prefix}mdm-subnet"
   }
 }
 
-resource "aws_subnet" "AppsSubnet7" {
+resource "aws_subnet" "int_dashboard" {
   vpc_id                  = "${aws_vpc.appsvpc.id}"
-  cidr_block              = "${var.vpc_subnet7_cidr_block}"
+  cidr_block              = "${var.int_dashboard_cidr_block}"
   map_public_ip_on_launch = true
   availability_zone       = "${var.az}"
 
   tags {
-    Name = "${local.name_prefix}subnet7"
+    Name = "${local.name_prefix}int-dashboard-subnet"
   }
 }
 
-resource "aws_subnet" "AppsSubnet8" {
+resource "aws_subnet" "ext_dashboard" {
   vpc_id                  = "${aws_vpc.appsvpc.id}"
-  cidr_block              = "${var.vpc_subnet8_cidr_block}"
+  cidr_block              = "${var.ext_dashboard_cidr_block}"
   map_public_ip_on_launch = true
   availability_zone       = "${var.az}"
 
   tags {
-    Name = "${local.name_prefix}subnet8"
+    Name = "${local.name_prefix}ext-dashboard-subnet"
   }
 }
