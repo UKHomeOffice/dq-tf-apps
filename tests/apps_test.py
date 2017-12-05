79246a8e6201cf5f0c6e6b771cba27797eb534dc
david-wirelab <info@wirelab.co.uk>
2017-12-05 10:42:32 +0000

    passed fmt

--- tests/apps_test.py
+++ tests/apps_test.py
@@ -23,10 +23,6 @@ class TestE2E(unittest.TestCase):
 
               cidr_block                  = "10.1.0.0/16"
               public_subnet_cidr_block    = "10.1.0.0/24"
-              dqdb_apps_cidr_block        = "10.1.2.0/24"
-              mdm_apps_cidr_block         = "10.1.10.0/24"
-              int_dashboard_cidr_block    = "10.1.12.0/24"
-              ext_dashboard_cidr_block    = "10.1.14.0/24"
               az                          = "eu-west-2a"
               name_prefix                 = "dq-"
             }
@@ -42,32 +38,8 @@ class TestE2E(unittest.TestCase):
     def test_apps_public_subnet_cidr_block(self):
         self.assertEqual(self.result['apps']["aws_subnet.public_subnet"]["cidr_block"], "10.1.0.0/24")
 
-    def test_apps_dqdb_apps_cidr_block(self):
-        self.assertEqual(self.result['apps']["aws_subnet.dqdb_apps"]["cidr_block"], "10.1.2.0/24")
-
-    def test_apps_mdm_apps_cidr_block(self):
-        self.assertEqual(self.result['apps']["aws_subnet.mdm_apps"]["cidr_block"], "10.1.10.0/24")
-
-    def test_apps_int_dashboard_cidr_block(self):
-        self.assertEqual(self.result['apps']["aws_subnet.int_dashboard"]["cidr_block"], "10.1.12.0/24")
-
-    def test_apps_ext_dashboard_cidr_block(self):
-        self.assertEqual(self.result['apps']["aws_subnet.ext_dashboard"]["cidr_block"], "10.1.14.0/24")
-
     def test_az_public_subnet(self):
         self.assertEqual(self.result['apps']["aws_subnet.public_subnet"]["availability_zone"], "eu-west-2a")
-
-    def test_az_dqdb_apps(self):
-        self.assertEqual(self.result['apps']["aws_subnet.dqdb_apps"]["availability_zone"], "eu-west-2a")
-
-    def test_az_mdm_apps(self):
-        self.assertEqual(self.result['apps']["aws_subnet.mdm_apps"]["availability_zone"], "eu-west-2a")
-
-    def test_az_int_dashboard(self):
-        self.assertEqual(self.result['apps']["aws_subnet.int_dashboard"]["availability_zone"], "eu-west-2a")
-
-    def test_az_ext_dashboard(self):
-        self.assertEqual(self.result['apps']["aws_subnet.ext_dashboard"]["availability_zone"], "eu-west-2a")
 
     def test_name_prefix_AppsRouteToInternet(self):
         self.assertEqual(self.result['apps']["aws_internet_gateway.AppsRouteToInternet"]["tags.Name"], "dq-apps-igw")
@@ -77,18 +49,6 @@ class TestE2E(unittest.TestCase):
 
     def test_name_prefix_public_subnet(self):
         self.assertEqual(self.result['apps']["aws_subnet.public_subnet"]["tags.Name"], "dq-apps-public-subnet")
-
-    def test_name_prefix_dqdb_apps(self):
-        self.assertEqual(self.result['apps']["aws_subnet.dqdb_apps"]["tags.Name"], "dq-apps-dqdb-subnet")
-
-    def test_name_prefix_mdm_apps(self):
-        self.assertEqual(self.result['apps']["aws_subnet.mdm_apps"]["tags.Name"], "dq-apps-mdm-subnet")
-
-    def test_name_prefix_int_dashboard(self):
-        self.assertEqual(self.result['apps']["aws_subnet.int_dashboard"]["tags.Name"], "dq-apps-int-dashboard-subnet")
-
-    def test_name_prefix_ext_dashboard(self):
-        self.assertEqual(self.result['apps']["aws_subnet.ext_dashboard"]["tags.Name"], "dq-apps-ext-dashboard-subnet")
 
     def test_name_prefix_appsnatgw(self):
         self.assertEqual(self.result['apps']["aws_nat_gateway.appsnatgw"]["tags.Name"], "dq-apps-natgw")
