5989ea044e3173808f944f8e9b983481ee401d17
david-wirelab <info@wirelab.co.uk>
2017-12-07 11:39:58 +0000

    fix fmt

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ resource "aws_route_table" "apps_route_table" {
   vpc_id = "${aws_vpc.appsvpc.id}"
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = "${aws_nat_gateway.appsnatgw.id}"
   }
 
