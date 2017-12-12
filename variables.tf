604bf3291297595c33304697e9bf0680a9fea387
david-wirelab <info@wirelab.co.uk>
2017-12-12 15:35:03 +0000

    adding placeholder variables for peering

--- variables.tf
+++ variables.tf
@@ -2,3 +2,10 @@ variable "cidr_block" {}
 variable "public_subnet_cidr_block" {}
 variable "az" {}
 variable "name_prefix" {}
+variable "peeringvpc_cidr" {}
+variable "opsvpc_cidr" {}
+variable "acpops_cidr_block" {}
+variable "acpcicd_cidr_block" {}
+variable "acpprod_cidr_block" {}
+variable "acpvpn_cidr_block" {}
+variable "vpc_peering_to_peering_id" {}
