2da4c8cc4413c76ee79dbbf77e535fbe4e16dc79
david-wirelab <info@wirelab.co.uk>
2017-12-13 16:51:56 +0000

    route config and vpc variable

--- variables.tf
+++ variables.tf
@@ -3,6 +3,7 @@ variable "public_subnet_cidr_block" {}
 variable "az" {}
 variable "name_prefix" {}
 variable "vpc_peering_to_peering_id" {}
+variable "vpc_peering_to_ops_id" {}
 
 variable "route_table_cidr_blocks" {
   description = "Map of CIDR blocks for the Apps private route table."
