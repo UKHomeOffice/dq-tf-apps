86900439b4beb44cde1bc5f667de16cf6c9e35c7
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-12-14 16:01:27 +0000

    output the subnet cidr block

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "appsvpc_cidr_block" {
   value = "${var.cidr_block}"
 }
 
+output "appssubnet_cidr_block" {
+  value = "${var.public_subnet_cidr_block}"
+}
+
 output "apps_natgw_id" {
   value = "${aws_nat_gateway.appsnatgw.id}"
 }
