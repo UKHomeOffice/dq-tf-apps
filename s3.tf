92cbed464d40b66ec13bf521ae0d9d957a01d317
david-wirelab <info@wirelab.co.uk>
2017-12-28 15:44:37 +0000

    rename kms key, assign buckets to master key, remove 2nd vpc endpoint

--- s3.tf
+++ s3.tf
@@ -6,16 +6,9 @@ resource "random_string" "bucket_string" {
   lower   = true
 }
 
-resource "aws_kms_key" "log_archive_bucket_key" {
+resource "aws_kms_key" "bucket_key" {
   description             = "This key is used to encrypt Archive Log bucket objects"
   deletion_window_in_days = 7
-
-  tags = {
-    Name             = "s3-${var.service}-log-archive-kms-key-${var.environment}"
-    Service          = "${var.service}"
-    Environment      = "${var.environment}"
-    EnvironmentGroup = "${var.environment_group}"
-  }
 }
 
 resource "aws_s3_bucket" "log_archive_bucket" {
@@ -26,7 +19,7 @@ resource "aws_s3_bucket" "log_archive_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.log_archive_bucket_key.arn}"
+        kms_master_key_id = "${aws_kms_key.bucket_key.arn}"
         sse_algorithm     = "aws:kms"
       }
     }
@@ -66,18 +59,6 @@ resource "aws_iam_policy" "log_archive_bucket_policy" {
 EOF
 }
 
-resource "aws_kms_key" "data_archive_bucket_key" {
-  description             = "This key is used to encrypt Archive Log bucket objects"
-  deletion_window_in_days = 7
-
-  tags = {
-    Name             = "s3-${var.service}-data-archive-kms-key-${var.environment}"
-    Service          = "${var.service}"
-    Environment      = "${var.environment}"
-    EnvironmentGroup = "${var.environment_group}"
-  }
-}
-
 resource "aws_s3_bucket" "data_archive_bucket" {
   bucket = "${var.s3_bucket_name["archive_data"]}-${random_string.bucket_string.result}"
   acl    = "${var.s3_bucket_acl["archive_data"]}"
@@ -86,7 +67,7 @@ resource "aws_s3_bucket" "data_archive_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.data_archive_bucket_key.arn}"
+        kms_master_key_id = "${aws_kms_key.bucket_key.arn}"
         sse_algorithm     = "aws:kms"
       }
     }
@@ -126,18 +107,6 @@ resource "aws_iam_policy" "data_archive_bucket_policy" {
 EOF
 }
 
-resource "aws_kms_key" "data_working_bucket_key" {
-  description             = "This key is used to encrypt Archive Log bucket objects"
-  deletion_window_in_days = 7
-
-  tags = {
-    Name             = "s3-${var.service}-data-working-kms-key-${var.environment}"
-    Service          = "${var.service}"
-    Environment      = "${var.environment}"
-    EnvironmentGroup = "${var.environment_group}"
-  }
-}
-
 resource "aws_s3_bucket" "data_working_bucket" {
   bucket = "${var.s3_bucket_name["working_data"]}-${random_string.bucket_string.result}"
   acl    = "${var.s3_bucket_acl["working_data"]}"
@@ -146,7 +115,7 @@ resource "aws_s3_bucket" "data_working_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.data_working_bucket_key.arn}"
+        kms_master_key_id = "${aws_kms_key.bucket_key.arn}"
         sse_algorithm     = "aws:kms"
       }
     }
@@ -186,18 +155,6 @@ resource "aws_iam_policy" "data_working_bucket_policy" {
 EOF
 }
 
-resource "aws_kms_key" "data_landing_bucket_key" {
-  description             = "This key is used to encrypt Archive Log bucket objects"
-  deletion_window_in_days = 7
-
-  tags = {
-    Name             = "s3-${var.service}-data-landing-kms-key-${var.environment}"
-    Service          = "${var.service}"
-    Environment      = "${var.environment}"
-    EnvironmentGroup = "${var.environment_group}"
-  }
-}
-
 resource "aws_s3_bucket" "data_landing_bucket" {
   bucket = "${var.s3_bucket_name["landing_data"]}-${random_string.bucket_string.result}"
   acl    = "${var.s3_bucket_acl["landing_data"]}"
@@ -206,7 +163,7 @@ resource "aws_s3_bucket" "data_landing_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.data_landing_bucket_key.arn}"
+        kms_master_key_id = "${aws_kms_key.bucket_key.arn}"
         sse_algorithm     = "aws:kms"
       }
     }
@@ -246,13 +203,7 @@ resource "aws_iam_policy" "data_landing_bucket_policy" {
 EOF
 }
 
-resource "aws_vpc_endpoint" "data_s3_endpoint" {
-  vpc_id          = "${aws_vpc.appsvpc.id}"
-  route_table_ids = ["${aws_route_table.apps_route_table.id}"]
-  service_name    = "com.amazonaws.eu-west-2.s3"
-}
-
-resource "aws_vpc_endpoint" "logs_s3_endpoint" {
+resource "aws_vpc_endpoint" "s3_endpoint" {
   vpc_id          = "${aws_vpc.appsvpc.id}"
   route_table_ids = ["${aws_route_table.apps_route_table.id}"]
   service_name    = "com.amazonaws.eu-west-2.s3"
