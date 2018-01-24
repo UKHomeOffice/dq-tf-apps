fd764e81a13e34ae5833b22218d259a06c1d553b
david-wirelab <info@wirelab.co.uk>
2018-01-24 15:45:20 +0000

    remove data-landing bucket from apps

--- s3.tf
+++ s3.tf
@@ -159,56 +159,6 @@ resource "aws_s3_bucket_policy" "data_working_bucket_policy" {
 EOF
 }
 
-resource "aws_s3_bucket" "data_landing_bucket" {
-  bucket = "${var.s3_bucket_name["landing_data"]}"
-  acl    = "${var.s3_bucket_acl["landing_data"]}"
-  region = "${var.region}"
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.bucket_key.arn}"
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-
-  versioning {
-    enabled = true
-  }
-
-  tags = {
-    Name = "s3-data-landing-bucket-${local.naming_suffix}"
-  }
-}
-
-resource "aws_s3_bucket_policy" "data_landing_bucket_policy" {
-  bucket = "${aws_s3_bucket.data_landing_bucket.id}"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {"Service": "ec2.amazonaws.com"},
-      "Action": "s3:ListBucket",
-      "Resource": ["${aws_s3_bucket.data_landing_bucket.arn}"]
-    },
-    {
-      "Effect": "Allow",
-      "Principal": {"Service": "ec2.amazonaws.com"},
-      "Action": [
-        "s3:PutObject",
-        "s3:GetObject"
-      ],
-      "Resource": ["${aws_s3_bucket.data_landing_bucket.arn}/*"]
-    }
-  ]
-}
-EOF
-}
-
 resource "aws_vpc_endpoint" "s3_endpoint" {
   vpc_id          = "${aws_vpc.appsvpc.id}"
   route_table_ids = ["${aws_route_table.apps_route_table.id}"]
