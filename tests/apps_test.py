08dadeba12cb8841382bbe922a568bcc48e709f2
Chris Nesbitt-Smith <chris@cns.me.uk>
2018-02-22 12:02:09 +0000

    add a realistic looking subnet for tests

--- tests/apps_test.py
+++ tests/apps_test.py
@@ -47,7 +47,7 @@ class TestE2E(unittest.TestCase):
 
               route_table_cidr_blocks     = {
                 peering_cidr = "1234"
-                ops_cidr = "1234"
+                ops_cidr = "10.2.0.0/24"
                 acp_vpn = "1234"
                 acp_prod = "1234"
                 acp_ops = "1234"
