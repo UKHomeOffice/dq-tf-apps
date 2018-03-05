06d390b46c0d76433df4d3c854e4d8b78df99142
david-wirelab <info@wirelab.co.uk>
2018-03-05 15:54:49 +0000

    update iam policy

--- iam_rmr.tf
+++ iam_rmr.tf
@@ -21,11 +21,27 @@ resource "aws_iam_group_policy" "rmr" {
   "Version": "2012-10-17",
   "Statement": [
     {
+      "Effect": "Allow",
+      "Action": "s3:ListBucket",
+      "Resource": "${aws_s3_bucket.data_archive_bucket.arn}"
+    },
+    {
       "Action": [
         "s3:GetObject"
       ],
       "Effect": "Allow",
-      "Resource": "${aws_s3_bucket.data_archive_bucket.arn}"
+      "Resource": "${aws_s3_bucket.data_archive_bucket.arn}/*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:DescribeKey"
+        ],
+        "Resource": "${aws_kms_key.bucket_key.arn}"
     }
   ]
 }
