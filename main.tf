a01268c8b6d8a6e5d92ad0e7fb4a6e4f225bdfa3
Duncan Mountford <duncan9562@gmail.com>
2018-11-05 14:34:30 +0000

    add bucket arn

--- main.tf
+++ main.tf
@@ -16,7 +16,8 @@ module "external_tableau" {
   az                           = "${var.az}"
   naming_suffix                = "${local.naming_suffix}"
   s3_archive_bucket_name       = "${aws_s3_bucket.data_archive_bucket.id}"
-  s3_archive_bucket_key        = "${aws_kms_key.bucket_key.arn}"  
+  s3_archive_bucket            = "${aws_s3_bucket.data_archive_bucket.arn}"
+  s3_archive_bucket_key        = "${aws_kms_key.bucket_key.arn}"
 }
 
 module "internal_tableau" {
