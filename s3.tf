104129de770d774ea2fa04103723d3a6fa2a6298
Duncan Mountford <duncan9562@gmail.com>
2018-11-09 11:31:43 +0000

    Remove bucket_key Policy

--- s3.tf
+++ s3.tf
@@ -1,37 +1,6 @@
 resource "aws_kms_key" "bucket_key" {
   description             = "This key is used to encrypt APPS buckets"
   deletion_window_in_days = 7
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Id": "key-default-1",
-    "Statement": [
-        {
-            "Sid": "Enable IAM User Permissions",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "${data.aws_caller_identity.current.arn}"
-            },
-            "Action": "kms:*",
-            "Resource": "*"
-        },
-        {
-            "Sid": "Allow services use of the key",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "s3.amazonaws.com"
-            },
-            "Action": [
-                "kms:Encrypt",
-                "kms:GenerateDataKey*",
-                "kms:DescribeKey"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-EOF
 }
 
 resource "aws_s3_bucket" "log_archive_bucket" {
