0ab7d9ea71f9ec915005b9740536d262589ad37d
david-wirelab <info@wirelab.co.uk>
2019-01-31 11:38:29 +0000

    add nats IAM user and permissions

---
+++ iam_nats.tf
@@ -0,0 +1,60 @@
+resource "aws_iam_group" "nats" {
+  name = "iam-group-nats-${local.naming_suffix}"
+}
+
+resource "aws_iam_group_membership" "nats" {
+  name = "iam-group-membership-nats-${local.naming_suffix}"
+
+  users = [
+    "${aws_iam_user.nats.name}",
+  ]
+
+  group = "${aws_iam_group.nats.name}"
+}
+
+resource "aws_iam_group_policy" "nats" {
+  name  = "group-policy-nats-${local.naming_suffix}"
+  group = "${aws_iam_group.nats.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "ListS3Bucket",
+      "Effect": "Allow",
+      "Action": "s3:ListBucket",
+      "Resource": "${aws_s3_bucket.data_archive_bucket.arn}"
+    },
+    {
+      "Sid": "PutS3Bucket",
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject"
+      ],
+      "Resource": "${aws_s3_bucket.data_archive_bucket.arn}/*"
+    },
+    {
+      "Sid": "UseKMSKey",
+      "Effect": "Allow",
+      "Action": [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:DescribeKey"
+        ],
+        "Resource": "${aws_kms_key.bucket_key.arn}"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user" "nats" {
+  name = "iam-user-nats-${local.naming_suffix}"
+}
+
+resource "aws_iam_access_key" "nats" {
+  user = "${aws_iam_user.nats.name}"
+}
