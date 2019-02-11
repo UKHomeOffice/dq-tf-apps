8174f0feb1b4f31c98df0ac479444f30a8c95da1
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-02-11 08:31:07 +0000

    terraform fmt

--- main.tf
+++ main.tf
@@ -123,15 +123,15 @@ module "lambda" {
 }
 
 module "airports_pipeline" {
-  source                       = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-airports-pipeline.git"
-  kms_key_s3                   = "${aws_kms_key.bucket_key.arn}"
-  lambda_subnet                = "${module.lambda.lambda_subnet}"
-  lambda_subnet_az2            = "${module.lambda.lambda_subnet_az2}"
-  lambda_sgrp                  = "${module.lambda.lambda_sgrp}"
-  rds_db_name                  = "${var.rds_db_name}"
-  rds_address = "${module.internal_tableau.rds_internal_tableau_address}"
-  naming_suffix                = "${local.naming_suffix}"
-  namespace                    = "${var.namespace}"
+  source            = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-airports-pipeline.git"
+  kms_key_s3        = "${aws_kms_key.bucket_key.arn}"
+  lambda_subnet     = "${module.lambda.lambda_subnet}"
+  lambda_subnet_az2 = "${module.lambda.lambda_subnet_az2}"
+  lambda_sgrp       = "${module.lambda.lambda_sgrp}"
+  rds_db_name       = "${var.rds_db_name}"
+  rds_address       = "${module.internal_tableau.rds_internal_tableau_address}"
+  naming_suffix     = "${local.naming_suffix}"
+  namespace         = "${var.namespace}"
 }
 
 module "airports_input_pipeline" {
