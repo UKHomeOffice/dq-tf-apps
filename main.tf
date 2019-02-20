4e169af00d635615613090646d202d532512149e
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-02-20 15:38:28 +0000

    Remove

--- main.tf
+++ main.tf
@@ -167,7 +167,6 @@ module "acl_input_pipeline" {
 module "reference_data_pipeline" {
   source         = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-reference-data-pipeline.git"
   kms_key_s3     = "${aws_kms_key.bucket_key.arn}"
-  pipeline_count = "${var.pipeline_count}"
   naming_suffix  = "${local.naming_suffix}"
   namespace      = "${var.namespace}"
 }
