8b6c2b9edc8655c14486dd408edc65fa54eea84a
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-02-25 14:24:55 +0000

    Add in vars required by lambda for rds

--- main.tf
+++ main.tf
@@ -207,10 +207,15 @@ module "gait_pipeline" {
 }
 
 module "internal_reporting_pipeline" {
-  source        = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-internal-reporting-pipeline.git"
-  kms_key_s3    = "${aws_kms_key.bucket_key.arn}"
-  naming_suffix = "${local.naming_suffix}"
-  namespace     = "${var.namespace}"
+  source                       = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-internal-reporting-pipeline.git"
+  lambda_subnet                = "${module.lambda.lambda_subnet}"
+  lambda_subnet_az2            = "${module.lambda.lambda_subnet_az2}"
+  lambda_sgrp                  = "${module.lambda.lambda_sgrp}"
+  rds_db_name                  = "${var.rds_db_name}"
+  rds_internal_tableau_address = "${module.internal_tableau.rds_internal_tableau_address}"
+  kms_key_s3                   = "${aws_kms_key.bucket_key.arn}"
+  naming_suffix                = "${local.naming_suffix}"
+  namespace                    = "${var.namespace}"
 }
 
 module "rds_deploy" {
