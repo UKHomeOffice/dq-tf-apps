8c3f2621ab11232f35f240b508a5f563be9410a3
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-02-25 13:12:56 +0000

    Fix

--- main.tf
+++ main.tf
@@ -209,7 +209,6 @@ module "gait_pipeline" {
 module "internal_reporting_pipeline" {
   source         = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-internal-reporting-pipeline.git"
   kms_key_s3     = "${aws_kms_key.bucket_key.arn}"
-  pipeline_count = "${var.pipeline_count}"
   naming_suffix  = "${local.naming_suffix}"
   namespace      = "${var.namespace}"
 }
