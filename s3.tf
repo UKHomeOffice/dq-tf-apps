f27b6c1727a53cb3ab1c02632ef79e1e0064d5c3
Pete Grant <pete.grant@digital.homeoffice.gov.uk>
2019-03-28 13:33:03 +0000

    Update key.

--- s3.tf
+++ s3.tf
@@ -847,8 +847,7 @@ resource "aws_s3_bucket" "api_internal_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.bucket_key.arn}"
-        sse_algorithm     = "aws:kms"
+        sse_algorithm     = "AES256"
       }
     }
   }
