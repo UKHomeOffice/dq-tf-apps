ca35efdc914f5f6df71c0d852c238f9b2faea14c
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-04-08 17:49:51 +0100

    Db name now passed in lambda payload; added fms address

--- main.tf
+++ main.tf
@@ -226,8 +226,8 @@ module "rds_deploy" {
   lambda_subnet                = "${module.lambda.lambda_subnet}"
   lambda_subnet_az2            = "${module.lambda.lambda_subnet_az2}"
   lambda_sgrp                  = "${module.lambda.lambda_sgrp}"
-  rds_db_name                  = "${var.rds_db_name}"
   rds_internal_tableau_address = "${module.internal_tableau.rds_internal_tableau_address}"
+  rds_fms_address              = "${module.fms.rds_address}"
   naming_suffix                = "${local.naming_suffix}"
   namespace                    = "${var.naming_suffix}"
 }
