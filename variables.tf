09746a2dea9d13d1687a764618c7c603324997be
Rhys Emmas <rhys.emmas@ntlworld.com>
2019-04-10 11:18:38 +0100

    test creating alarms for each db instance

--- variables.tf
+++ variables.tf
@@ -86,3 +86,48 @@ variable "s3_haproxy_config_bucket" {
 variable "s3_haproxy_config_bucket_key" {
   description = "Haproxy config bucket KMS Key ARN"
 }
+
+#########################################
+# begin rds instance monitoring variables
+#########################################
+
+variable "db_instance_id" {
+  description = "The instance ID of the RDS database instance that you want to monitor."
+  type        = "list"
+}
+
+variable "cpu_utilization_threshold" {
+  description = "The maximum percentage of CPU utilization."
+  type        = "string"
+  default     = 80
+}
+
+variable "disk_queue_depth_threshold" {
+  description = "The maximum number of outstanding IOs (read/write requests) waiting to access the disk."
+  type        = "string"
+  default     = 64
+}
+
+variable "freeable_memory_threshold" {
+  description = "The minimum amount of available random access memory in Byte."
+  type        = "string"
+  default     = 64000000
+
+  # 64 Megabyte in Bytes
+}
+
+variable "free_storage_space_threshold" {
+  description = "The minimum amount of available storage space in Byte."
+  type        = "string"
+  default     = 2000000000
+
+  # 2 Gigabyte in Bytes
+}
+
+variable "swap_usage_threshold" {
+  description = "The maximum amount of swap space used on the DB instance in Byte."
+  type        = "string"
+  default     = 256000000
+
+  # 256 Megabyte in Bytes
+}
