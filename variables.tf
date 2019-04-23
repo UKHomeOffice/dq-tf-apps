37fe1d8f9659a44d5d2ba2215e27fbd85fa2b7fa
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-04-23 18:34:52 +0100

    Add drt to athena

--- variables.tf
+++ variables.tf
@@ -54,6 +54,7 @@ variable "dq_pipeline_ops_readwrite_database_name_list" {
     "api_input",
     "oag_transform",
     "internal_reporting",
+    "drt"
   ]
 }
 
@@ -72,6 +73,7 @@ variable "dq_pipeline_ops_readwrite_bucket_list" {
     "s3-dq-cross-record-scored",
     "s3-dq-raw-file-index-internal",
     "s3-dq-athena-log",
+    "s3-dq-drt-working"
   ]
 }
 
