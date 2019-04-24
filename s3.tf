340870a996d8518561bb0aca074b4e04a20c6d32
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-04-24 10:56:56 +0100

    Switch from kms to aes256 to prevent throttling by the kms service

--- s3.tf
+++ s3.tf
@@ -1382,8 +1382,7 @@ resource "aws_s3_bucket" "drt_working_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.bucket_key.arn}"
-        sse_algorithm     = "aws:kms"
+        sse_algorithm = "AES256"
       }
     }
   }
