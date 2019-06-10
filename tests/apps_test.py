33786c7845f701bfbe5d65664fe0d3ff8a721a6b
david-wirelab <info@wirelab.co.uk>
2019-06-10 15:33:24 +0100

    Fix test module

--- tests/apps_test.py
+++ tests/apps_test.py
@@ -488,16 +488,16 @@ class TestE2E(unittest.TestCase):
         self.assertEqual(self.result['apps']['ops_pipeline']["aws_cloudwatch_log_group.lambda_log_group_cleaner"]["tags.Name"], "lambda-log-group-cleaner-ops-apps-preprod-dq")
 
     def test_name_suffix_s3count_iam_group(self):
-        self.assertEqual(self.result['apps']['apps']["aws_iam_group.s3count"]["name"], "iam-group-s3count-apps-preprod-dq")
+        self.assertEqual(self.result['apps']["aws_iam_group.s3count"]["name"], "iam-group-s3count-apps-preprod-dq")
 
     def test_name_suffix_s3count_iam_group_membership(self):
-        self.assertEqual(self.result['apps']['apps']["aws_iam_group_membership.s3count"]["name"], "iam-group-membership-s3count-apps-preprod-dq")
+        self.assertEqual(self.result['apps']["aws_iam_group_membership.s3count"]["name"], "iam-group-membership-s3count-apps-preprod-dq")
 
     def test_name_suffix_s3count_iam_group_policy(self):
-        self.assertEqual(self.result['apps']['apps']["aws_iam_group_policy.s3count"]["name"], "iam-group-policy-s3count-apps-preprod-dq")
+        self.assertEqual(self.result['apps']["aws_iam_group_policy.s3count"]["name"], "iam-group-policy-s3count-apps-preprod-dq")
 
     def test_name_suffix_s3count_iam_user(self):
-        self.assertEqual(self.result['apps']['apps']["aws_iam_user.s3count"]["name"], "iam-user-s3count-apps-preprod-dq")
+        self.assertEqual(self.result['apps']["aws_iam_user.s3count"]["name"], "iam-user-s3count-apps-preprod-dq")
 
 if __name__ == '__main__':
     unittest.main()
