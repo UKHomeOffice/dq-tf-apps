ebd4c0f7ef5e34ddd84f845ec523be647d7f0db3
Duncan Mountford <duncan9562@gmail.com>
2019-06-12 13:36:20 +0100

    Changes to IAM policy

--- iam_data_archive.tf
+++ iam_data_archive.tf
@@ -50,3 +50,92 @@ resource "aws_iam_group_membership" "data_archive_bucket" {
 
   group = "${aws_iam_group.data_archive_bucket.name}"
 }
+
+resource "aws_iam_user" "cdp-s4-data" {
+  name = "cdp-s4-data"
+}
+
+resource "aws_iam_access_key" "cdp-s4-data" {
+  user = "cdp-s4-data"
+}
+
+resource "aws_iam_group" "cdp-s4-data" {
+  name = "cdp-s4-data"
+}
+
+resource "aws_iam_group_policy" "cdp-s4-data" {
+  name  = "cdp-s4-data"
+  group = "cdp-s4-data"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "s3:ListBucket",
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.data_archive_bucket.arn}",
+      "Condition":{"StringEquals":{"s3:prefix":[
+        "",
+        "s4/parsed/2018/",
+        "s4/parsed/2018/06/",
+        "s4/parsed/2018/06/16/",
+        "s4/parsed/2018/06/17/",
+        "s4/parsed/2018/06/18/",
+        "s4/parsed/2018/06/19/",
+        "s4/parsed/2018/06/20/",
+        "s4/parsed/2018/06/21/",
+        "s4/parsed/2018/06/22/",
+        "s4/parsed/2018/06/23/",
+        "s4/parsed/2018/06/24/",
+        "s4/parsed/2018/06/25/",
+        "s4/parsed/2018/06/26/",
+        "s4/parsed/2018/06/27/",
+        "s4/parsed/2018/06/28/",
+        "s4/parsed/2018/06/29/",
+        "s4/parsed/2018/06/30/"],
+        "s3:delimiter":["/"]}}
+    },
+    {
+      "Action": "s3:GetObject",
+      "Effect": "Allow",
+      "Resource": ["${aws_s3_bucket.data_archive_bucket.arn}/s4/parsed/2018/06/16/*",
+        "${aws_s3_bucket.data_archive_bucket.arn}/s4/parsed/2018/06/17/*",
+        "${aws_s3_bucket.data_archive_bucket.arn}/s4/parsed/2018/06/18/*",
+        "${aws_s3_bucket.data_archive_bucket.arn}/s4/parsed/2018/06/19/*",
+        "${aws_s3_bucket.data_archive_bucket.arn}/s4/parsed/2018/06/20/*",
+        "${aws_s3_bucket.data_archive_bucket.arn}/s4/parsed/2018/06/21/*",
+        "${aws_s3_bucket.data_archive_bucket.arn}/s4/parsed/2018/06/22/*",
+        "${aws_s3_bucket.data_archive_bucket.arn}/s4/parsed/2018/06/23/*",
+        "${aws_s3_bucket.data_archive_bucket.arn}/s4/parsed/2018/06/24/*",
+        "${aws_s3_bucket.data_archive_bucket.arn}/s4/parsed/2018/06/25/*",
+        "${aws_s3_bucket.data_archive_bucket.arn}/s4/parsed/2018/06/26/*",
+        "${aws_s3_bucket.data_archive_bucket.arn}/s4/parsed/2018/06/27/*",
+        "${aws_s3_bucket.data_archive_bucket.arn}/s4/parsed/2018/06/28/*",
+        "${aws_s3_bucket.data_archive_bucket.arn}/s4/parsed/2018/06/29/*",
+        "${aws_s3_bucket.data_archive_bucket.arn}/s4/parsed/2018/06/30/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:DescribeKey"
+        ],
+        "Resource": "${aws_kms_key.bucket_key.arn}"
+      }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_group_membership" "cdp-s4-data" {
+  name = "cdp-s4-data"
+
+  users = ["cdp-s4-data"]
+
+  group = "cdp-s4-data"
+}
