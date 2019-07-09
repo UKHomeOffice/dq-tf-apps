6d0ef5d4bcb396e733ec0eae15fd572137fbef40
Olaleye Alex-Oni <olaleyealex-oni@DQDEVLAP994.local>
2019-07-09 14:20:56 +0100

    git@ entry taken out

--- main.tf
+++ main.tf
@@ -307,7 +307,7 @@ module "freight_glue" {
 }
 
 module "dailytasks" {
-  source        = "git@github.com:UKHomeOffice/dq-tf-apps.git"
+  source        = "github.com:UKHomeOffice/dq-tf-apps.git"
   naming_suffix = "${local.naming_suffix}"
   namespace     = "${var.namespace}"
 }
