fe3d3dd722e4603f43b513dec7467fbcf3792ab3
James Lizamore <jwlizamore@gmail.com>
2019-09-16 15:26:13 +0100

    Remove up to date API data access from BFID bucket list

--- variables.tf
+++ variables.tf
@@ -92,14 +92,12 @@ variable "dq_pipeline_ops_readonly_bucket_list" {
 
 variable "dq_pipeline_ops_freight_readwrite_bucket_list" {
   default = ["s3-dq-freight-archive",
-    "s3-dq-api-internal",
     "s3-dq-athena-log",
   ]
 }
 
 variable "dq_pipeline_ops_freight_readwrite_database_name_list" {
   default = ["freight",
-    "api_input",
   ]
 }
 
