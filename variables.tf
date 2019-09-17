4c923cf1278228c602794ef98763c4676fec0ffb
Duncan Mountford <duncan9562@gmail.com>
2019-09-17 10:40:45 +0100

    Add comma

--- variables.tf
+++ variables.tf
@@ -81,7 +81,7 @@ variable "dq_pipeline_ops_readwrite_bucket_list" {
     "s3-dq-reporting-internal-working",
     "s3-dq-airports-working",
     "s3-dq-airports-internal",
-    "s3-dq-carrier-portal-working"
+    "s3-dq-carrier-portal-working",
     "s3-dq-fms-working",
   ]
 }
