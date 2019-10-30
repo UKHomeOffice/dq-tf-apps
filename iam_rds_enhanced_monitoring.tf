648d114d6429f84fa4a86968027c3b9bf0be24b1
Duncan Mountford <duncan9562@gmail.com>
2019-10-30 12:42:27 +0000

    Add RDS Enhanced Monitoring role

---
+++ iam_rds_enhanced_monitoring.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "rds_enhanced_monitoring_role" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "cloudwatch.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "rds_enhanced_monitoring_role" {
+  role = "${aws_iam_role.rds_enhanced_monitoring_role.id}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+}
