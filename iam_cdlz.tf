4992492314ed1cf7eadfdd0a128268a71a177f33
Jwanderema <wanderema@hotmail.co.uk>
2019-11-19 12:49:13 +0000

    cdlz initial policy

--- iam_cdlz.tf
+++ iam_cdlz.tf
@@ -12,7 +12,7 @@ resource "aws_iam_group_membership" "cdlz" {
   group = "${aws_iam_group.cdlz.name}"
 }
 
-resource "aws_iam_group_policy" "cdlz_freight" {
+resource "aws_iam_group_policy" "cdlz" {
   name  = "group-policy-cdlz-${local.naming_suffix}"
   group = "${aws_iam_group.cdlz.id}"
 
@@ -35,47 +35,6 @@ resource "aws_iam_group_policy" "cdlz_freight" {
                 "s3:DeleteObject"
             ],
             "Resource": "arn:aws:s3:::s3-dq-freight-archive-prod/archive/*"
-        },
-        {
-            "Sid": "UseKMSKey",
-            "Effect": "Allow",
-            "Action": [
-                "kms:Encrypt",
-                "kms:Decrypt",
-                "kms:ReEncrypt*",
-                "kms:GenerateDataKey*",
-                "kms:DescribeKey"
-            ],
-            "Resource": "arn:aws:kms:eu-west-2:337779336338:key/ae75113d-f4f6-49c6-a15e-e8493fda0453"
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_iam_group_policy" "cdlz_api" {
-  name  = "group-policy-cdlz-${local.naming_suffix}"
-  group = "${aws_iam_group.cdlz.id}"
-
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "ListS3Bucket",
-            "Effect": "Allow",
-            "Action": "s3:ListBucket",
-            "Resource": "arn:aws:s3:::s3-dq-api-archive-prod"
-        },
-        {
-            "Sid": "PutS3Bucket",
-            "Effect": "Allow",
-            "Action": [
-                "s3:GetObject",
-                "s3:PutObject",
-                "s3:DeleteObject"
-            ],
-            "Resource": "arn:aws:s3:::s3-dq-api-archive-prod/*"
         },
         {
             "Sid": "UseKMSKey",
