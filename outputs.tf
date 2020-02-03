4790e1b9cc4c58c9e40d37fed4576d397e58d945
David Takacs <info@wirelab.co.uk>
2020-02-03 10:26:58 +0000

    remove freight resources

--- outputs.tf
+++ outputs.tf
@@ -53,11 +53,3 @@ output "dq_pipeline_ops_readwrite_bucket_list" {
 output "dq_pipeline_ops_readonly_bucket_list" {
   value = "${var.dq_pipeline_ops_readonly_bucket_list}"
 }
-
-output "dq_pipeline_ops_freight_readwrite_bucket_list" {
-  value = "${var.dq_pipeline_ops_freight_readwrite_bucket_list}"
-}
-
-output "dq_pipeline_ops_freight_readwrite_database_name_list" {
-  value = "${var.dq_pipeline_ops_freight_readwrite_database_name_list}"
-}
