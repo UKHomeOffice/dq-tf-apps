bb44f1e0acc7e27ffa0b17002c1fe3a00a6da4ff
Haitham Abou Houly <haithamabouhouly@hodqadms-MacBook-Pro.local>
2020-07-28 14:09:19 +0100

    YEL-3236 comment out api-input pipline

--- main.tf
+++ main.tf
@@ -168,13 +168,13 @@ module "cdlz" {
   namespace     = var.namespace
 }
 
-module "api_input_pipeline" {
-  source        = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-api-input-pipeline.git"
-  kms_key_s3    = aws_kms_key.bucket_key.arn
-  lambda_slack  = module.ops_pipeline.lambda_slack
-  naming_suffix = local.naming_suffix
-  namespace     = var.namespace
-}
+# module "api_input_pipeline" {
+#   source        = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-api-input-pipeline.git"
+#   kms_key_s3    = aws_kms_key.bucket_key.arn
+#   lambda_slack  = module.ops_pipeline.lambda_slack
+#   naming_suffix = local.naming_suffix
+#   namespace     = var.namespace
+# }
 
 module "api_record_level_score_pipeline" {
   source        = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-api-record-level-score-pipeline.git"
