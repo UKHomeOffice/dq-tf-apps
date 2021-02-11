781e16b4eb305e2771255d725800535d8d90c26b
Haitham Abou Houly <haithamabouhouly@hodqadms-mbp.lan>
2021-02-11 10:55:12 +0000

    add access to fedat_reporting

--- variables.tf
+++ variables.tf
@@ -134,6 +134,7 @@ variable "dq_pipeline_ops_readwrite_bucket_list" {
     "s3-dq-carrier-portal-working",
     "s3-dq-fms-working",
     "s3-dq-snsgb-internal",
+    "s3-dq-asn-internal",
   ]
 }
 
