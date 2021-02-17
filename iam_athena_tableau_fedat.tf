e9fdbcd390153c0fa5b7c0fc98a4a010eeee3d36
JWanderema <wanderema@hotmail.co.uk>
2021-02-17 16:43:48 +0000

    add user

--- iam_athena_tableau_fedat.tf
+++ iam_athena_tableau_fedat.tf
@@ -1,3 +1,7 @@
 resource "aws_iam_user" "athena_tableau_fedat" {
   name = "iam-user-athena-tableau-fedat-${local.naming_suffix}"
 }
+
+resource "aws_iam_access_key" "athena_tableau_fedat" {
+  user = aws_iam_user.athena_tableau_fedat.name
+}
