e880dc59915c718dec626d680c5bab42bb3fb7d9
JWanderema <wanderema@hotmail.co.uk>
2021-02-17 16:45:47 +0000

    add ssm

--- iam_athena_tableau_fedat.tf
+++ iam_athena_tableau_fedat.tf
@@ -5,3 +5,15 @@ resource "aws_iam_user" "athena_tableau_fedat" {
 resource "aws_iam_access_key" "athena_tableau_fedat" {
   user = aws_iam_user.athena_tableau_fedat.name
 }
+
+resource "aws_ssm_parameter" "athena_tableau_fedat_id" {
+  name  = "tableau-athena-fedat-user-id-${local.naming_suffix}"
+  type  = "SecureString"
+  value = aws_iam_access_key.athena_tableau_fedat.id
+}
+
+resource "aws_ssm_parameter" "athena_tableau_fedat_key" {
+  name  = "tableau-athena-fedat-user-key-${local.naming_suffix}"
+  type  = "SecureString"
+  value = aws_iam_access_key.athena_tableau_fedat.secret
+}
