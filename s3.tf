d6e807de3a37d2e44f275b493c8a6d5348be168f
JWanderema <wanderema@hotmail.co.uk>
2021-04-14 16:23:58 +0100

    add data generator bucket

--- s3.tf
+++ s3.tf
@@ -2756,6 +2756,64 @@ resource "aws_s3_bucket_metric" "dq_rm_internal_bucket_logging" {
   name   = "dq_rm_internal_metric"
 }
 
+resource "aws_s3_bucket" "dq_data_generator_bucket" {
+  bucket = var.s3_bucket_name["dq_data_generator"]
+  acl    = var.s3_bucket_acl["dq_data_generator"]
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  logging {
+    target_bucket = aws_s3_bucket.log_archive_bucket.id
+    target_prefix = "dq_data_generator/"
+  }
+
+  tags = {
+    Name = "s3-dq-data-generator-${local.naming_suffix}"
+  }
+}
+
+resource "aws_s3_bucket_policy" "dq_data_generator_bucket_policy" {
+  bucket = var.s3_bucket_name["dq_data_generator"]
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "HTTP",
+      "Effect": "Deny",
+      "Principal": "*",
+      "Action": "*",
+      "Resource": "arn:aws:s3:::${var.s3_bucket_name["dq_data_generator"]}/*",
+      "Condition": {
+        "Bool": {
+          "aws:SecureTransport": "false"
+        }
+      }
+    }
+  ]
+}
+POLICY
+
+  depends_on = [aws_s3_bucket.dq_data_generator_bucket]
+
+}
+
+resource "aws_s3_bucket_metric" "dq_data_generator_bucket_logging" {
+  bucket = var.s3_bucket_name["dq_data_generator"]
+  name   = "dq_data_generator_metric"
+}
+
 resource "aws_vpc_endpoint" "s3_endpoint" {
   vpc_id          = aws_vpc.appsvpc.id
   route_table_ids = [aws_route_table.apps_route_table.id]
