3a71a150f1ba7eda78e8143e3fc10478d349f70e
JWanderema <wanderema@hotmail.co.uk>
2021-04-21 10:44:45 +0100

    add envs to iam

--- tests/apps_test.py
+++ tests/apps_test.py
@@ -87,7 +87,7 @@ class TestE2E(unittest.TestCase):
                 dq_asn_marine_internal        = "abcd"
                 dq_rm_archive                 = "abcd"
                 dq_rm_internal                = "abcd"
-                # dq_data_generator             = "abcd"
+                dq_data_generator             = "abcd"
 
               }
 
@@ -140,7 +140,7 @@ class TestE2E(unittest.TestCase):
                 dq_asn_marine_internal        = "private"
                 dq_rm_archive                 = "private"
                 dq_rm_internal                = "private"
-                # dq_data_generator             = "private"
+                dq_data_generator             = "private"
               }
 
               route_table_cidr_blocks     = {
