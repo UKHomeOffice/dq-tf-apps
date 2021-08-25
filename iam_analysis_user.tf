67d0f4c577ee669c2551bbe7483d2ed343a125a1
JWanderema <wanderema@hotmail.co.uk>
2021-08-25 14:14:28 +0100

    add user for analysis

---
+++ iam_analysis_user.tf
@@ -0,0 +1,56 @@
+#This User allows the certificate expiry script to access slack_notification_webhook on ssm
+
+resource "aws_iam_group_policy" "analysis_proxy_user_policy" {
+  name  = "analysis_proxy_user_policy"
+  group = aws_iam_group.analysis_proxy_user.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ssm:GetParameter"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "arn:aws:ssm:eu-west-2:*:parameter/slack_notification_webhook"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user" "analysis_proxy_user" {
+  name = "analysis_proxy_user"
+}
+
+
+resource "aws_iam_access_key" "analysis_proxy_user" {
+  user = aws_iam_user.analysis_proxy_user.name
+}
+
+resource "aws_iam_group" "analysis_proxy_user" {
+  name = "analysis_proxy_user"
+}
+
+resource "aws_iam_group_membership" "analysis_proxy_user" {
+  name = "analysis_proxy_user"
+
+  users = [aws_iam_user.analysis_proxy_user.name]
+
+  group = aws_iam_group.analysis_proxy_user.name
+}
+
+resource "aws_ssm_parameter" "analysis_proxy_user_id" {
+  name  = "analysis-proxy-user-id-${local.naming_suffix}"
+  type  = "SecureString"
+  value = aws_iam_access_key.analysis_proxy_user.id
+}
+
+resource "aws_ssm_parameter" "analysis_proxy_user_key" {
+  name  = "analysis-proxy-user-key-${local.naming_suffix}"
+  type  = "SecureString"
+  value = aws_iam_access_key.analysis_proxy_user.secret
+}
