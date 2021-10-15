ba49d85e8924d298700b7924c9bf0305b4185bf8
JWanderema <wanderema@hotmail.co.uk>
2021-10-15 13:16:21 +0100

    add snsgb user

---
+++ iam_snsgb_archive.tf
@@ -0,0 +1,83 @@
+resource "aws_iam_policy" "dq_snsgb_archive_bucket_policy" {
+  name = "dq_snsgb_archive_bucket_policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetBucketLocation",
+        "s3:ListBucket",
+        "s3:ListBucketMultipartUploads",
+        "s3:ListMultipartUploadParts"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.dq_snsgb_archive_bucket.arn}"
+      ]
+    },
+    {
+      "Action": [
+        "s3:PutObject"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.dq_snsgb_archive_bucket.arn}/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:DescribeKey"
+        ],
+        "Resource": [
+          "${aws_s3_bucket.dq_snsgb_archive_bucket.arn}"
+        ]
+      }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_group_policy_attachment" "dq_snsgb_archive_bucket" {
+  group      = aws_iam_group.dq_snsgb_archive_bucket.name
+  policy_arn = aws_iam_policy.dq_snsgb_archive_bucket_policy.arn
+}
+
+resource "aws_iam_user" "dq_snsgb_archive_bucket" {
+  name = "dq_snsgb_archive_bucket_user"
+}
+
+
+resource "aws_iam_access_key" "dq_snsgb_archive_bucket" {
+  user = aws_iam_user.dq_snsgb_archive_bucket.name
+}
+
+resource "aws_iam_group" "dq_snsgb_archive_bucket" {
+  name = "dq_snsgb_archive_bucket"
+}
+
+resource "aws_iam_group_membership" "dq_snsgb_archive_bucket" {
+  name = "dq_snsgb_archive_bucket"
+
+  users = [aws_iam_user.dq_snsgb_archive_bucket.name]
+
+  group = aws_iam_group.dq_snsgb_archive_bucket.name
+}
+
+resource "aws_ssm_parameter" "dq_snsgb_archive_bucket_id" {
+  name  = "snsgb-archive-bucket-user-id-${local.naming_suffix}"
+  type  = "SecureString"
+  value = aws_iam_access_key.dq_snsgb_archive_bucket.id
+}
+
+resource "aws_ssm_parameter" "dq_snsgb_archive_bucket_key" {
+  name  = "snsgb-archive-bucket-user-key-${local.naming_suffix}"
+  type  = "SecureString"
+  value = aws_iam_access_key.dq_snsgb_archive_bucket.secret
+}
