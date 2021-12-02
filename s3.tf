e277a3e74e99652ab77b35e427e9492e784a6e7c
JWanderema <wanderema@hotmail.co.uk>
2021-12-02 14:16:11 +0000

    add bucket

--- s3.tf
+++ s3.tf
@@ -2877,6 +2877,64 @@ resource "aws_s3_bucket_metric" "dq_gait_landing_staging_bucket_logging" {
   name   = "dq_gait_landing_staging_metric"
 }
 
+resource "aws_s3_bucket" "dq_pnr_archive_bucket" {
+  bucket = var.s3_bucket_name["dq_pnr_archive"]
+  acl    = var.s3_bucket_acl["dq_pnr_archive"]
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  logging {
+    target_bucket = aws_s3_bucket.log_archive_bucket.id
+    target_prefix = "dq_pnr_archive/"
+  }
+
+  tags = {
+    Name = "s3-dq-pnr-archive-${local.naming_suffix}"
+  }
+}
+
+resource "aws_s3_bucket_policy" "dq_pnr_archive_bucket_policy" {
+  bucket = var.s3_bucket_name["dq_pnr_archive"]
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "HTTP",
+      "Effect": "Deny",
+      "Principal": "*",
+      "Action": "*",
+      "Resource": "arn:aws:s3:::${var.s3_bucket_name["dq_pnr_archive"]}/*",
+      "Condition": {
+        "Bool": {
+          "aws:SecureTransport": "false"
+        }
+      }
+    }
+  ]
+}
+POLICY
+
+  depends_on = [aws_s3_bucket.dq_pnr_archive_bucket]
+
+}
+
+resource "aws_s3_bucket_metric" "dq_pnr_archive_bucket_logging" {
+  bucket = var.s3_bucket_name["dq_pnr_archive"]
+  name   = "dq_pnr_archive_metric"
+}
+
 resource "aws_vpc_endpoint" "s3_endpoint" {
   vpc_id          = aws_vpc.appsvpc.id
   route_table_ids = [aws_route_table.apps_route_table.id]
