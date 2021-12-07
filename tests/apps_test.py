505ce2712e7fc48296671e3a42209cd322500542
JWanderema <wanderema@hotmail.co.uk>
2021-12-07 10:10:40 +0000

    add varaibles to role attachment

--- tests/apps_test.py
+++ tests/apps_test.py
@@ -37,6 +37,7 @@ class TestE2E(unittest.TestCase):
               s3_httpd_config_bucket_key      = "arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab"
               haproxy_config_bucket           = "s3-bucket-name"
               haproxy_config_bucket_key       = "arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab"
+              account_id                      = {"preprod" = "235678"}
 
               s3_bucket_name = {
                 archive_log                   = "abcd"
