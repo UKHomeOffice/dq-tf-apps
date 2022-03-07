9850fc6043432a714c10bff991f97025f6a85420
haitham.abou-houly <haitham.abou-houly@digital.homeoffice.gov.uk>
2022-03-07 13:53:26 +0000

    modify ahena_log bucket policy

--- s3.tf
+++ s3.tf
@@ -1338,11 +1338,9 @@ resource "aws_s3_bucket_policy" "athena_log_policy" {
           ],
           Condition = {
              NotIpAddress = {
-              aws:SourceIp = (
+              aws:SourceIp =
               var.environment == "prod" ?
-              [ var.dq_ips_prod ] :
-              [ var.dq_ips_notprod ]
-              )
+              "${var.dq_ips_prod}"  : "${var.dq_ips_notprod}"
 
              },
              "Bool": {
