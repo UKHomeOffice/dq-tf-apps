51b0f36cadf9fef4c671e2e6f84fe4aa3a86fcc0
haitham.abou-houly <haitham.abou-houly@digital.homeoffice.gov.uk>
2022-08-11 11:48:21 +0100

    remove rhel ami from data

--- data.tf
+++ data.tf
@@ -17,21 +17,21 @@ data "aws_ami" "win" {
 data "aws_availability_zones" "available" {
 }
 
-data "aws_ami" "rhel" {
-  most_recent = true
-
-  filter {
-    name = "name"
-
-    values = [
-      "RHEL-7.4*",
-    ]
-  }
-
-  owners = [
-    "309956199498",
-  ]
-}
+# data "aws_ami" "rhel" {
+#   most_recent = true
+#
+#   filter {
+#     name = "name"
+#
+#     values = [
+#       "RHEL-7.4*",
+#     ]
+#   }
+#
+#   owners = [
+#     "309956199498",
+#   ]
+# }
 
 data "aws_caller_identity" "current" {
 }
@@ -42,4 +42,3 @@ data "aws_region" "current" {
 data "aws_kms_key" "glue" {
   key_id = "alias/aws/glue"
 }
-
