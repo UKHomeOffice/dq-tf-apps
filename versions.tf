a8a54396e9e7cbd6bbe5e7b7ed9385e7a4ba22bd
Ian Relf <ian.relf@digital.homeoffice.gov.uk>
2023-07-28 14:28:10 +0100

    Copy lambda directory structure to the temp dir too

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 
 terraform {
-  required_version = ">= 1.5.3"
+  required_version = "~> 1.5.3"
   required_providers {
-    archive = ">= 2.4.0"
-    aws     = ">= 5.9.0"
-    random  = ">= 3.5.0"
+    archive = "~> 2.4.0"
+    aws     = "~> 5.9.0"
+    random  = "~> 3.5.0"
   }
 }
