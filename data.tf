23ca915cfd88e32a3e1124e61c489d365941893f
Mustafa Deerow <mdeerow@hodqadms-MacBook-Pro-4.local>
2023-10-10 11:15:37 +0100

    remove data.tf file

--- data.tf
+++ data.tf
@@ -1,44 +1,44 @@
-data "aws_ami" "win" {
-  most_recent = true
-
-  filter {
-    name = "name"
-
-    values = [
-      "Windows_Server-2012-R2_RTM-English-64Bit-Base-*",
-    ]
-  }
-
-  owners = [
-    "amazon",
-  ]
-}
-
-data "aws_availability_zones" "available" {
-}
-
-# data "aws_ami" "rhel" {
+# data "aws_ami" "win" {
 #   most_recent = true
-#
+
 #   filter {
 #     name = "name"
-#
+
 #     values = [
-#       "RHEL-7.4*",
+#       "Windows_Server-2012-R2_RTM-English-64Bit-Base-*",
 #     ]
 #   }
-#
+
 #   owners = [
-#     "309956199498",
+#     "amazon",
 #   ]
 # }
 
-data "aws_caller_identity" "current" {
-}
+# data "aws_availability_zones" "available" {
+# }
 
-data "aws_region" "current" {
-}
+# # data "aws_ami" "rhel" {
+# #   most_recent = true
+# #
+# #   filter {
+# #     name = "name"
+# #
+# #     values = [
+# #       "RHEL-7.4*",
+# #     ]
+# #   }
+# #
+# #   owners = [
+# #     "309956199498",
+# #   ]
+# # }
 
-data "aws_kms_key" "glue" {
-  key_id = "alias/aws/glue"
-}
+# data "aws_caller_identity" "current" {
+# }
+
+# data "aws_region" "current" {
+# }
+
+# data "aws_kms_key" "glue" {
+#   key_id = "alias/aws/glue"
+# }
