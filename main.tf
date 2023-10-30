54e4c2f2c758d9036ba9fd7ad5bfaecedf1b8ff9
Srikanth Arunachalam <116078089+s-arunachalam@users.noreply.github.com>
2023-10-30 13:52:34 +0000

    feature/yel-8015: minimise overly Permissive Security Group Rules

--- main.tf
+++ main.tf
@@ -39,8 +39,8 @@ module "internal_tableau" {
   source                                = "github.com/UKHomeOffice/dq-tf-internal-tableau"
   acp_prod_ingress_cidr                 = "10.5.0.0/16"
   dq_ops_ingress_cidr                   = var.route_table_cidr_blocks["ops_cidr"]
-  dq_internal_dashboard_subnet_cidr     = "10.1.12.0/24"
-  dq_internal_dashboard_subnet_cidr_az2 = "10.1.13.0/24"
+  dq_internal_dashboard_subnet_cidr     = "10.1.12.0/27"
+  dq_internal_dashboard_subnet_cidr_az2 = "10.1.13.0/27"
   peering_cidr_block                    = "10.3.0.0/16"
   apps_vpc_id                           = aws_vpc.appsvpc.id
   route_table_id                        = aws_route_table.apps_route_table.id
